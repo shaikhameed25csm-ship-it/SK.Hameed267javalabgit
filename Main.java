8c5632d139342fc76516e7d2bb7efc25c5cb9e1c
SK.Hameed267javalabgit <shaikhameed.25.csm@anits.edu.in>
2026-09-17 14:24:40 +0530

    master

---
+++ Main.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class main {
+public static void main (Strint[] args) {
+System.out.Println("Hello World");
+}
+}
---
+++ My Package/main.java
@@ -0,0 +1,9 @@
+import MyPackage.*;
+
+public class main {
+	public static void main(String[] args) {
+		file o = new file ();
+		o.display();
+	}
+	
+}
